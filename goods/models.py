b72785b2aacb6bcca415f709b5017ae34e765b08
KiTKaT2034 <daniil.golovinoff@yandex.ru>
2024-03-26 19:40:24 +0500

    start work in DB

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,32 @@
+from email.policy import default
+from enum import unique
+from tabnanny import verbose
+from unicodedata import category, digit
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slig = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slig = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    discripton = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+    
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
--- goods/views.py
+++ goods/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from django.template import context
 
 # Create your views here.
 def catalog(request):
-    return render(request, 'goods/products.html')
+    context: dict[str, str] = {
+        'title': 'Home - Каталог',
+        'goods':  [
+            {
+                'name': 'название',
+                'price': '250',
+                'sale': 'no',
+            },
+
+            {
+                'name': 'название',
+                'price': '250',
+                'sale': 'no',
+            },
+            {
+                'name': 'название',
+                'price': '250',
+                'sale': 'no',
+            },
+            
+            
+        ]
+    }
+    return render(request, 'goods/products.html', context)
 
 def product(request):
     return render(request, 'goods/product_page.html')

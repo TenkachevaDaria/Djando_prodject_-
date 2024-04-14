4ee81d23c5fa11d409550ea54206dfc5bda41b19
KiTKaT2034 <daniil.golovinoff@yandex.ru>
2024-04-14 22:42:17 +0500

    search products in all pages

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -2,7 +2,16 @@ from django import template
 from django.utils.http import urlencode
 
 
+from goods.models import Product
+
+
 register = template.Library()
+
+
+@register.simple_tag()
+def tag_products():
+    return Product.objects.all()
+
 
 @register.simple_tag(takes_context=True)
 def change_params(context, **kwargs):
--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,5 @@
-from django.db.models import Q
+from django.contrib.postgres.search import SearchRank, SearchQuery, SearchVector
+
 
 from goods.models import Product
 
@@ -6,12 +7,7 @@ from goods.models import Product
 def q_search(query):
     if query.isdigit() and len(query) <= 2:
         return Product.objects.filter(id=int(query))
-    
-    keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
-
-    for token in keywords:
-        q_objects |= Q(name__icontains=token)
-
-    return Product.objects.filter(q_objects)
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Product.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")

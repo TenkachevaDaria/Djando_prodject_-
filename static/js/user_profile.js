f060eeaaed429b6a82d5b0087c9c7c13592e21f8
KiTKaT2034 <daniil.golovinoff@yandex.ru>
2024-05-05 17:46:45 +0500

    current page designe

--- static/js/user_profile.js
+++ static/js/user_profile.js
@@ -37,6 +37,19 @@ document.addEventListener("DOMContentLoaded", function() {
     hidePopupButton.addEventListener('click', closePopup);
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const userProducts = document.querySelector('.user_products_liked');
+    const popupUserProducts = document.querySelector('.popup_user_products_liked');
+    const hidePopupButton = popupUserProducts.querySelector('.hide_popup_user_products_liked');
+    function openPopup() {
+        popupUserProducts.style.display = 'block';
+    }
+    function closePopup() {
+        popupUserProducts.style.display = 'none';
+    }
+    userProducts.addEventListener('click', openPopup);
+    hidePopupButton.addEventListener('click', closePopup);
+});
 
 document.addEventListener("DOMContentLoaded", function() {
     const userProducts = document.querySelector('.user_history_bought_box');

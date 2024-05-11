3475d4391a1efaa0513bef737100ba69801c2c2c
KiTKaT2034 <daniil.golovinoff@yandex.ru>
2024-05-11 17:19:48 +0500

    combining filters in the catalog

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -18,3 +18,10 @@ def change_params(context, **kwargs):
     query = context['request'].GET.dict()
     query.update(kwargs)
     return urlencode(query)
+
+@register.simple_tag(takes_context=True)
+def combine_filters(context, **kwargs):
+    print('сработало')
+    querye = context['request'].GET.dict()
+    querye.update(kwargs)
+    return urlencode(querye)

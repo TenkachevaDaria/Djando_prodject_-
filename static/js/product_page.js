0cafc150361bad1fcc81b89037e1dcd76ccb8004
KiTKaT2034 <daniil.golovinoff@yandex.ru>
2024-05-11 11:26:48 +0500

    fixed a problem with the search in the mobile version of the site

--- static/js/product_page.js
+++ static/js/product_page.js
@@ -2,10 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttonShowMore = document.querySelector('.btn_show_more');
     const usersList = document.querySelector('.users_list_reviews');
     const reviews = Array.from(usersList.querySelectorAll('.another_user_review'));
-
+    
     reviews.slice(3).forEach(review => {
         review.style.display = 'none';
     });
+
+    if (reviews.length <= 3) {
+        buttonShowMore.style.display = 'none';
+    }
+
     buttonShowMore.addEventListener('click', function() {
         reviews.slice(3).forEach(review => {
             review.style.display = 'block';
--- static/js/search_product.js
+++ static/js/search_product.js
@@ -54,3 +54,62 @@ searchInput.addEventListener('input', function() {
         dropListContainer.style.display = 'none';
     }
 });
+
+
+// мобильный
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInputMobile = document.querySelector('input[name="q_mobile"]');
+    const productListMobile = document.querySelector('.products_searchers_list_mobile');
+    const productItemsMobile = productListMobile.querySelectorAll('.product_mini_card_mobile');
+    const showMoreButtonMobile = document.querySelector('.drop_list__btn_mobile');
+
+    let visibleItemCountMobile = 5;
+    const totalItems_mobile = productItemsMobile.length;
+
+    function filterProducts_mobile(searchTerm) {
+        let visibleItemsCount_mobile = 0;
+
+        productItemsMobile.forEach(function(item) {
+            const productNameMobile = item.querySelector('.product_name_mobile').textContent.trim().toLowerCase();
+            if (productNameMobile.includes(searchTerm)) {
+                if (visibleItemsCount_mobile < visibleItemCountMobile) {
+                    item.style.display = 'block';
+                    visibleItemsCount_mobile++;
+                } else {
+                    item.style.display = 'none';
+                }
+            } else {
+                item.style.display = 'none';
+            }
+        });
+
+        if (visibleItemsCount_mobile < totalItems_mobile) {
+            showMoreButtonMobile.style.display = 'block';
+        } else {
+            showMoreButtonMobile.style.display = 'none';
+        }
+    }
+
+    searchInputMobile.addEventListener('input', function() {
+        const searchTermMobile = this.value.trim().toLowerCase();
+        filterProducts_mobile(searchTermMobile);
+    });
+
+    showMoreButtonMobile.addEventListener('click', function() {
+        visibleItemCountMobile += 5;
+        const searchTermMobile = searchInputMobile.value.trim().toLowerCase();
+        filterProducts_mobile(searchTermMobile);
+    });
+
+    const dropListContainerMobile = document.querySelector('.drop_list_container_mobile');
+
+    searchInputMobile.addEventListener('focus', function() {
+        dropListContainerMobile.style.display = 'block';
+    });
+
+    searchInputMobile.addEventListener('blur', function() {
+        setTimeout(function() {
+            dropListContainerMobile.style.display = 'none';
+        }, 100);
+    });
+});
